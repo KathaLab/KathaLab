2f4801c91390dbbd0dfaa05992741c3014599bca
xernois <antoine2496@gmail.com>
2022-10-05 00:08:35 +0200

    init

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { PageTest } from "./pages/pageTest/PageTest";
+
+function render() {
+  ReactDOM.render(<PageTest />, document.body);
+}
+
+render();
---
+++ webpack.rules.js
@@ -0,0 +1,38 @@
+module.exports = [
+  // Add support for native node modules
+  {
+    // We're specifying native_modules in the test because the asset relocator loader generates a
+    // "fake" .node file which is really a cjs file.
+    test: /native_modules\/.+\.node$/,
+    use: "node-loader",
+  },
+  {
+    test: /\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: "@vercel/webpack-asset-relocator-loader",
+      options: {
+        outputAssetBase: "native_modules",
+      },
+    },
+  },
+  {
+    test: /\.tsx?$/,
+    exclude: /(node_modules|\.webpack)/,
+    use: {
+      loader: "ts-loader",
+      options: {
+        transpileOnly: true,
+      },
+    },
+  },
+  {
+    test: /\.scss$/,
+    use: [
+      { loader: "style-loader" },
+      { loader: "css-modules-typescript-loader" }, // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td"
+      { loader: "css-loader", options: { modules: true } },
+      { loader: "sass-loader" },
+    ],
+  },
+];
