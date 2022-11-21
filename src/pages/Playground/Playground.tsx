82937c95f363f4f414fafeb2305557da1beeabf4
Benjamin <benjamin.rodrigues2@etu.unilim.fr>
2022-11-21 09:55:26 +0100

    import dialog, electronAPI

--- src/pages/Playground/Playground.tsx
+++ src/pages/Playground/Playground.tsx
@@ -3,11 +3,17 @@ import {Pages} from "../../app";
 import {Language, LocalizationName} from "../../localization";
 import LocalizationContext from "../../context/LocalizationContext";
 import styles from "./Playground.module.scss"
+//import {dialog} from '@electron/remote'
+//import * as fs from "fs";
+
+/*
 type componentType = {
     switchPage: (page: Pages) => void
-}
 
-export const Playground = ({switchPage}: componentType) => {
+}
+*/
+export const Playground = ({switchPage}) => {
+
 
     const {updateContext, languageDico} = useContext(LocalizationContext);
 
@@ -19,9 +25,45 @@ export const Playground = ({switchPage}: componentType) => {
         switchPage(Pages.Gallery)
     }
 
+    /*
+    const readFile = (filepath: fs.PathOrFileDescriptor) => {
+        fs.readFile(filepath, 'utf-8',  (err)  => {
+            if(err){
+                alert("An error ocurred reading the file :" + err.message);
+                return;
+            }
+        });
+    }
+    */
+
+    const test = () => {
+        console.log(window.dialog)
+
+    }
+
+
+/*
+    const open = () => {
+
+        dialog.showOpenDialog({
+            properties: ['openFile']
+        }).then(function (response) {
+            if (response === undefined) {
+                console.log("y'a r");
+                return;
+            } else {
+                console.log(response.filePaths, "path");
+            }
+        });
+    }
+*/
+
+
     return <>
         <h1 className={styles.titre} >{languageDico[LocalizationName.titlePlayground]}</h1>
         <button onClick={handlePage}>{languageDico[LocalizationName.titleGallery]}</button>
         <button onClick={handleLocalization}>switch</button>
+
+        <button >Lire un fichier</button>
     </>
 }
