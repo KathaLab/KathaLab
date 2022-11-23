1c61cf76086ee3e6e6b984b460bf7c634b904d6b
xernois <antoine2496@gmail.com>
2022-11-23 17:25:47 +0100

    fix bug start

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useEffect, useState } from "react";
 
 // importing the two pages
-import { Gallery, Playground, Settings } from "./pages"
+import { Gallery, Playground, Settings } from "./pages";
 
 // importing stuff related to the localization
 import localizationContext from "./context/LocalizationContext";
@@ -10,7 +10,7 @@ import { Language, LanguageToLocalization } from "./localization";
 import { createRoot } from "react-dom/client";
 
 // importing styles
-import themes from "./theme/_theme.scss"
+import themes from "./theme/_theme.scss";
 import themeContext from "./context/ThemeContext";
 import { TitleBar } from "./components/TitleBar/TitleBar";
 
@@ -20,53 +20,67 @@ import { SnackBar, snackBarMessageType } from "./components/SnackBar/SnackBar";
 import { useDelayQueue } from "./hooks/useDelayQueue";
 import { Lab } from "./model/Lab";
 
-export type themeNames = keyof typeof themes
+export type themeNames = keyof typeof themes;
 export enum Pages {
-    Gallery,
-    Playground,
-    Settings,
+  Gallery,
+  Playground,
+  Settings,
 }
 
 const App = () => {
-    const [page, setPage] = useState<Pages>(Pages.Playground)
-    const [localization, setLocalization] = useState<Language>(Language.EN)
-    const [theme, setTheme] = useState<themeNames>("theme-dark2")
-    const [snackbarVisibility, setSnackbarVisibility] = useState(false)
+  const [page, setPage] = useState<Pages>(Pages.Gallery);
+  const [localization, setLocalization] = useState<Language>(Language.EN);
+  const [theme, setTheme] = useState<themeNames>("theme-dark2");
+  const [snackbarVisibility, setSnackbarVisibility] = useState(false);
 
-    const labRef = React.useRef<Lab | null>(null);
+  const labRef = React.useRef<Lab | null>(null);
 
-    const switchPage = (page: Pages, option?: { lab?: Lab }) => {
-        labRef.current = option?.lab ?? null;
-        setPage(page);
-    }
-    
-    // handle snackbar
-    const handleSnackBarMessage = (message: snackBarMessageType) => {
-        setSnackbarVisibility(true)
-        return new Promise<void>((resolve) => setTimeout(() => {
-            setSnackbarVisibility(false)
-            setTimeout(resolve, 200);
-        }, message.duration - 200))
-    }
-    const [[currentElement], addElement] = useDelayQueue<snackBarMessageType>(handleSnackBarMessage);
+  const switchPage = (page: Pages, option?: { lab?: Lab }) => {
+    labRef.current = option?.lab ?? null;
+    setPage(page);
+  };
 
-    return <main className={themes[theme]}>
-        <localizationContext.Provider value={{ language: localization, languageDico: LanguageToLocalization[localization], updateContext: setLocalization }}>
-            <themeContext.Provider value={{ theme, updateContext: setTheme }}>
-                <SnackBarContext.Provider value={{ updateContext: addElement }}>
-                    <TitleBar switchPage={setPage}></TitleBar>
-                    <div className="pageWrapper">
-                        {
-                            page == Pages.Gallery ? <Gallery switchPage={setPage} />
-                                : page == Pages.Playground ? <Playground switchPage={setPage} />
-                                    : page == Pages.Settings ? <Settings switchPage={setPage} /> : null
-                        }
-                    </div>
-                    <SnackBar visibility={snackbarVisibility} {...currentElement} />
-                </SnackBarContext.Provider>
-            </themeContext.Provider>
-        </localizationContext.Provider>
+  // handle snackbar
+  const handleSnackBarMessage = (message: snackBarMessageType) => {
+    setSnackbarVisibility(true);
+    return new Promise<void>((resolve) =>
+      setTimeout(() => {
+        setSnackbarVisibility(false);
+        setTimeout(resolve, 200);
+      }, message.duration - 200)
+    );
+  };
+  const [[currentElement], addElement] = useDelayQueue<snackBarMessageType>(
+    handleSnackBarMessage
+  );
+
+  return (
+    <main className={themes[theme]}>
+      <localizationContext.Provider
+        value={{
+          language: localization,
+          languageDico: LanguageToLocalization[localization],
+          updateContext: setLocalization,
+        }}
+      >
+        <themeContext.Provider value={{ theme, updateContext: setTheme }}>
+          <SnackBarContext.Provider value={{ updateContext: addElement }}>
+            <TitleBar switchPage={setPage}></TitleBar>
+            <div className="pageWrapper">
+              {page == Pages.Gallery ? (
+                <Gallery switchPage={setPage} />
+              ) : page == Pages.Playground ? (
+                <Playground switchPage={setPage} />
+              ) : page == Pages.Settings ? (
+                <Settings switchPage={setPage} />
+              ) : null}
+            </div>
+            <SnackBar visibility={snackbarVisibility} {...currentElement} />
+          </SnackBarContext.Provider>
+        </themeContext.Provider>
+      </localizationContext.Provider>
     </main>
-}
+  );
+};
 
-createRoot(document.querySelector('#root')).render(<App />,);
+createRoot(document.querySelector("#root")).render(<App />);
--- src/electronAPI.ts
+++ src/electronAPI.ts
@@ -1,49 +1,61 @@
-import { app, dialog, ipcMain } from 'electron';
-import fs from 'fs';
-import { Lab } from './model/Lab';
+import { app, dialog, ipcMain } from "electron";
+import fs from "fs";
+import { Lab } from "./model/Lab";
 
 export class electronAPI {
-    initialize = async () => {
-
-        ipcMain.handle('dialog:open-file', async () => {
-            return await dialog.showOpenDialog({
-                properties: ['openFile']
-            }).then(response => {
-                return response.filePaths[0];
-            }).catch(err => {
-                console.warn(err.message)
-            })
+  initialize = async () => {
+    ipcMain.handle("dialog:open-file", async () => {
+      return await dialog
+        .showOpenDialog({
+          properties: ["openFile"],
         })
-        ipcMain.handle('dialog:open-directory', async () => {
-            return await dialog.showOpenDialog({
-                properties: ['createDirectory', "openDirectory"],
-            }).then(response => {
-                return response.filePaths[0];
-            }).catch(err => {
-                console.warn(err.message)
-            })
-                    ipcMain.handle('save:save', async (_, obj) => {
-            try {
-                fs.writeFileSync(app.getAppPath() + `/data/${obj.id}.json`, JSON.stringify(obj), 'utf-8');
-            }
-            catch (e) {
-                console.warn(e)
-            }
+        .then((response) => {
+          return response.filePaths[0];
         })
-        ipcMain.handle('save:load', async (event, name) => {
-            try {
-                const files = fs.readdirSync(app.getAppPath() + `/data`);
-
-                const lab: Lab[] = [];
-
-                files.forEach(file => {
-                    lab.push(JSON.parse(fs.readFileSync(app.getAppPath() + `/data/${file}`, 'utf-8')));
-                })
-                event.sender.send('save:load', lab);
-            }
-            catch (e) {
-                console.warn(e)
-            }
+        .catch((err) => {
+          console.warn(err.message);
+        });
+    });
+    ipcMain.handle("dialog:open-directory", async () => {
+      return await dialog
+        .showOpenDialog({
+          properties: ["createDirectory", "openDirectory"],
         })
-    }
+        .then((response) => {
+          return response.filePaths[0];
+        })
+        .catch((err) => {
+          console.warn(err.message);
+        });
+    });
+    ipcMain.handle("save:save", async (_, obj) => {
+      try {
+        fs.writeFileSync(
+          app.getAppPath() + `/data/${obj.id}.json`,
+          JSON.stringify(obj),
+          "utf-8"
+        );
+      } catch (e) {
+        console.warn(e);
+      }
+    });
+    ipcMain.handle("save:load", async (event, name) => {
+      try {
+        const files = fs.readdirSync(app.getAppPath() + `/data`);
+
+        const lab: Lab[] = [];
+
+        files.forEach((file) => {
+          lab.push(
+            JSON.parse(
+              fs.readFileSync(app.getAppPath() + `/data/${file}`, "utf-8")
+            )
+          );
+        });
+        event.sender.send("save:load", lab);
+      } catch (e) {
+        console.warn(e);
+      }
+    });
+  };
 }
