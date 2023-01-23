4b24b112d70cd576cb7bc44f989dcaa3dc29a862
benjamin <benjamin.rodrigues2@etu.unilim.fr>
2023-01-23 14:31:03 +0100

    ip, cidr, is_up, collision domain is now not obligatory parameter for Interface

--- src/model/Interfaces.tsx
+++ src/model/Interfaces.tsx
@@ -2,9 +2,9 @@ import {Route} from "./Route";
 
 export type Interfaces = {
   interfaceName: string
-  ip: string
-  cidr: number
-  is_up: boolean
-  collision_domain: string
+  ip?: string
+  cidr?: number
+  is_up?: boolean
+  collision_domain?: string
   routes?: Route[]
 }
