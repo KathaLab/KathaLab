b69c24f9a64f8eb34d2ab892ec967d2760650196
benjamin <benjamin.rodrigues2@etu.unilim.fr>
2023-02-08 22:52:13 +0100

    fix regex

--- src/lib/RegexConst.ts
+++ src/lib/RegexConst.ts
@@ -1,5 +1,3 @@
-//export const LAB_CONF_REGEX = /(lab_name ?= ?(?<name>.{0,30}))|(lab_email=(?<email>.{0,50}))|(lab_web=(?<web>.{0,255}))|(lab_version=(?<version>.{0,30}))|(lab_author=(?<author>.{0,128}))|(lab_description=(?<description>.{0,500}))/gim;
-
 export const LAB_NAME_REGEX =/lab_name ?= ?(?<name>.{0,32})/gi;
 export const LAB_AUTHOR_REGEX =/lab_author ?= ?(?<author>.{0,128})/gi;
 export const LAB_DESCRIPTION_REGEX = /lab_description ?= ?(?<description>.{0,512})/gi;
@@ -7,6 +5,7 @@ export const LAB_EMAIL_REGEX = /lab_email ?= ?(?<email>.{0,64})/gi;
 export const LAB_WEB_REGEX = /lab_web ?= ?(?<web>.{0,128})/gi;
 export const LAB_VERSION_REGEX = /lab_version ?= ?(?<version>.{0,32})/gi;
 
+export const LAB_DEVICE_NAME_REGEX = /(?<deviceName>\w{1,32})\[\w+]=.*;?/gi;
 export const LAB_DEVICE_BRIDGED_REGEX = /\w{1,32}\[bridged]=(?<bridged>\w+)/gi;
 export const LAB_DEVICE_IMAGE_REGEX = /\w{1,32}\[image]=(?<image>.+)/gi;
 export const LAB_DEVICE_MEMORY_REGEX = /\w{1,32}\[memory]=(?<memory>.+)/gi;
@@ -25,15 +24,12 @@ export const LAB_DEVICE_INTERFACE_IP_REGEX = /ip +address +add +(\d{1,3}\.\d{1,3
 export const LAB_DEVICE_INTERFACE_NAME_FROM_IP_ADD = /ip +address +add +\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2} +dev +(\w{1,64}) ?|ip +addr +add +\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2} +dev +(\w{1,64}) ?|ip +a +add +\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2} +dev +(\w{1,64}) ?|ip +a +a +\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2} +dev +(\w{1,64}) ?|ip +addr +a +\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2} +dev +(\w{1,64}) ?/gi;
 export const LAB_DEVICE_INTERFACE_NAME_AND_IP_IS_UP_REGEX = /ip +link +set +up +dev +(\w{1,64})|ip +link +set +dev +(\w{1,64}) +up|ip +link +set +(\w{1,64}) +up|ip +link +set +up +(\w{1,64})/gi;
 export const LAB_DEVICE_INTERFACE_CIDR_REGEX = /ip +address +add +\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/(\d{1,2}) +dev +\w{1,64} ?|ip +addr +add +\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/(\d{1,2}) +dev +\w{1,64} ?|ip +a +add +\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/(\d{1,2}) +dev +\w{1,64} ?|ip +a +a +\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/(\d{1,2}) +dev +\w{1,64} ?|ip +addr +a +\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/(\d{1,2}) +dev +\w{1,64} ?/gi;
-
-export const LAB_DEVICE_INTERFACE_DEFAULT_ROUTE_REGEX =/a/gi;
-export const LAB_DEVICE_INTERFACE_CUSTOM_ROUTE_REGEX = /a/gi;
-
-export const LAB_DEVICE_NAME_REGEX = /(?<deviceName>\w{1,32})\[\w+]=.*;?/gi;
+//export const LAB_DEVICE_INTERFACE_DEFAULT_ROUTE_REGEX =/a/gi;
+//export const LAB_DEVICE_INTERFACE_CUSTOM_ROUTE_REGEX = /a/gi;
 
 export const DEVICE_IP = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/g
 export const DEVICE_CIDR = /^(?:(?:3[0-2]|2[0-9]|1[0-9]|[0-9]?))$/g;
 
 export const EXPORTED_LAB_NAME_REGEX = /^([\w-_ ]{1,32})$/gi
-export const EXPORTED_NAME_REGEX = /^(\w{1,32})$/gi
+export const EXPORTED_NAME_REGEX = /^([a-zA-Z0-9]{1,32})$/gi
 export const EXPORTED_NUMBER_VAR_REGEX = /^([0-9]+)$/gi
